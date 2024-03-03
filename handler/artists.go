e57f7f2fb051b3020c7f97f1eff291aace19b572
Vivek Singh <2307vivek@gmail.com>
2024-03-03 22:28:42 +0530

    scrape a single atist

--- handler/artists.go
+++ handler/artists.go
@@ -1,5 +1,29 @@
 package handler
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/2307vivek/song-lyrics/queue"
+	"github.com/2307vivek/song-lyrics/utils"
+	"github.com/gocolly/colly"
+)
+
 func ScrapeArtists() {
-	
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	c := utils.CreateColly(true, 20, 2 * time.Second)
+
+	c.OnHTML("#alfabetMusicList a.nameMusic[href]", func(h *colly.HTMLElement) {
+		songLink := h.Attr("href")
+		
+		fmt.Println("publishing", songLink)
+		queue.SongQ.Publish(ctx, []byte(songLink))
+	})
+
+	c.Visit("https://www.vagalume.com.br/eminem/")
+	c.Wait()
 }
