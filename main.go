cc929f4f4f8ca5d6be3563fae4cd3fb3aed3f28e
Vivek Singh <2307vivek@gmail.com>
2024-03-03 15:45:09 +0530

    created project structure

--- main.go
+++ main.go
@@ -1,72 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	//"time"
-
-	"github.com/PuerkitoBio/goquery"
-	"github.com/gocolly/colly"
+	"github.com/2307vivek/song-lyrics/handler"
 )
 
 func main() {
-	x := os.Args[0]
+	instance := os.Args[1]
 
-	fmt.Println(x)
-}
-
-func helper() {
-	c := colly.NewCollector()
-	c.Async = true
-
-	songs := make([]string, 0)
-	lyrics := make([]string, 0)
-
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("visiting", r.URL)
-	// })
-
-	c.OnHTML("#alfabetMusicList a.nameMusic[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		link = e.Request.AbsoluteURL(link)
-		songs = append(songs, link)
-	})
-
-	c.OnHTML("#lyrics", func(h *colly.HTMLElement) {
-
-		link := h.Request.URL.String()
-
-		fmt.Println("visiting", link)
-		lyrics = append(lyrics, link)
-
-		if link == "https://www.vagalume.com.br/eminem/zelda-rap--remix-.html" {
-			h.DOM.Contents().Each(func(i int, s *goquery.Selection) {
-				if goquery.NodeName(s) == "#text" {
-					fmt.Printf(">>> (%d) >>> %s\n", i, s.Text())
-				}
-			})
-		}
-	})
-
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Println("Got this error:", e)
-	})
-
-	// c.Limit(&colly.LimitRule{
-	// 	DomainGlob:  "*",
-	// 	Parallelism: 20,
-	// 	RandomDelay: 2 * time.Second,
-	// })
-
-	c.Visit("https://www.vagalume.com.br/eminem/zelda-rap--remix-.html")
-
-	c.Wait()
-
-	// for _, song := range songs {
-	// 	c.Visit(song)
-	// }
-	// c.Wait()
-
-	fmt.Println("songs length", len(songs))
-	fmt.Println("lyrics length", len(lyrics))
+	if instance == "lyrics" {
+		handler.ScrapeArtists()
+	} else if instance == "artists" {
+		handler.ScrapeArtists()
+	}
 }
---
+++ types/Artists.go
@@ -0,0 +1,7 @@
+package types
+
+type Artist struct {
+	Name string
+	Url string
+	Popularity int16
+}
