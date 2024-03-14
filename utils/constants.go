f40746500604746037a915a456176d3174f5be91
Vivek Singh <2307vivek@gmail.com>
2024-03-14 17:48:25 +0530

    added name for bloom filters

--- utils/constants.go
+++ utils/constants.go
@@ -2,3 +2,6 @@ package utils
 
 const SONG_QUEUE_NAME = "songs_queue"
 const ARTIST_QUEUE_NAME = "artists_queue"
+
+const SONG_BLOOM_FILTER_NAME = "project:songs"
+const ARTIST_BLOOM_FILTER_NAME = "project:artists"
