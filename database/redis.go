1270c4d4c65be8771a02df0dbf59008bfee5deaa
Vivek Singh <2307vivek@gmail.com>
2024-03-14 17:47:35 +0530

    created function to check and add

--- database/redis.go
+++ database/redis.go
@@ -1,15 +1,34 @@
 package database
 
 import (
+	"context"
+
 	"github.com/2307vivek/song-lyrics/utils"
 	"github.com/redis/go-redis/v9"
 )
 
 var Rdb *redis.Client
+var ctx context.Context
 
 func ConnectToRedis(url string) {
 	opt, err := redis.ParseURL(url)
 	utils.FailOnError(err, "Failed while connecting to redis.")
 
 	Rdb = redis.NewClient(opt)
+
+	createContext()
+}
+
+func CheckCache(key string, url string) bool {
+	exists :=  Rdb.BFExists(ctx, key, url).Val()
+	return exists
+}
+
+func AddToCache(key string, url string) bool {
+	isadded := Rdb.BFAdd(ctx, key, url).Val()
+	return isadded
+}
+
+func createContext() {
+	ctx = context.Background()
 }
