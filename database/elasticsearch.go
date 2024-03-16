fd35c33c4d1363782dd7ee1118344a86a24a1945
Vivek Singh <2307vivek@gmail.com>
2024-03-16 17:05:06 +0530

    created function to connect to elastic search

--- database/elasticsearch.go
+++ database/elasticsearch.go
@@ -2,13 +2,19 @@ package database
 
 import (
 	"github.com/2307vivek/song-lyrics/utils"
-	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v7"
 )
 
 var ES *elasticsearch.Client
 
-func ConnectToElasticSearch() {
-	client, err := elasticsearch.NewClient()
+func ConnectToElasticSearch(url string, username string, password string) {
+	client, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{
+			url,
+		},
+		Username: username,
+		Password: password,
+	})
 	utils.FailOnError(err, "Failed to connect to elastic search")
 
 	ES = client
