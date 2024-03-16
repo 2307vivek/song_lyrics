5ef29be9c5a97783cd7925f7cfd61a88fe3fb1de
Vivek Singh <2307vivek@gmail.com>
2024-03-17 00:40:50 +0530

    implemented function to flush remaining items

--- handler/song_store.go
+++ handler/song_store.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/2307vivek/song-lyrics/database"
 	"github.com/2307vivek/song-lyrics/types"
@@ -11,27 +12,34 @@ import (
 
 var SongLyricStore = make(chan types.SongLyrics, 50)
 
-func SongStore(count int) {
+func SongStore(batchSize int) {
 
 	counter := 0
 	songBuffer := bytes.NewBuffer(nil)
 
+	timer := time.NewTimer(20 * time.Second)
+
+	go func() {
+		for range timer.C {
+			if songBuffer.Len() != 0 {
+				flush(songBuffer)
+				counter = 0
+			}
+		}
+	}()
+
 	for song := range SongLyricStore {
+		timer.Reset(20 * time.Second)
+
 		songJson, err := json.Marshal(song)
 		if err != nil {
 			fmt.Println("failed to serialize song", song)
 		}
 		songJson = append(songJson, "\n"...)
 
-		if counter == count {
+		if counter == batchSize {
 			// flush to db
-			fmt.Printf("\"flushing to db\": %v\n", "flushing to db")
-			res, e := database.ES.Bulk(bytes.NewReader(songBuffer.Bytes()), database.ES.Bulk.WithIndex("sooong"))
-			if e != nil {
-				fmt.Println("failed to insert items", e)
-			}
-			res.Body.Close()
-			songBuffer.Reset()
+			flush(songBuffer)
 			counter = 0
 		} else {
 			meta := []byte(fmt.Sprintf(`{ "index" : { }%s`, "\n"))
@@ -40,4 +48,14 @@ func SongStore(count int) {
 			counter++
 		}
 	}
+}
+
+func flush(buffer *bytes.Buffer) {
+	fmt.Printf("\"flushing to db\": %v\n", "flushing to db")
+	res, e := database.ES.Bulk(bytes.NewReader(buffer.Bytes()), database.ES.Bulk.WithIndex("sooong"))
+	if e != nil {
+		fmt.Println("failed to insert items", e)
+	}
+	res.Body.Close()
+	buffer.Reset()
 }
