08b6474c26f209dec5e256a7be44d70b54947ff8
Vivek Singh <2307vivek@gmail.com>
2024-03-17 12:04:34 +0530

    creted function to get queue details

---
+++ queue/queue_details.go
@@ -0,0 +1,48 @@
+package queue
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/2307vivek/song-lyrics/types"
+)
+
+func GetQueueDetails(queueName string, vhost string) (types.QueueDetails, error) {
+
+	queueUrl := os.Getenv("RABBITMQ_ADDRESS") + "/api/queues/" + vhost + "/" + queueName
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", queueUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+	}
+
+	// Set basic authentication header
+	req.SetBasicAuth(os.Getenv("RABBITMQ_USERNAME"), os.Getenv("RABBITMQ_PASSWORD"))
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err) 
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+	}
+
+	queueDetails := &types.QueueDetails{}
+	if resp.Status == "200 OK" {
+		err := json.Unmarshal(body, queueDetails)
+		if err != nil {
+			fmt.Println("Error unmarshaing:", err)
+		}
+	}
+	return *queueDetails, err
+}
