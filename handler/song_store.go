5511d58b65b2d976ff128489f8cb38d7291875fe
Vivek Singh <2307vivek@gmail.com>
2024-03-18 18:02:03 +0530

    created index global variable

--- handler/song_store.go
+++ handler/song_store.go
@@ -18,12 +18,14 @@ func SongStore(batchSize int) {
 	counter := 0
 	songBuffer := bytes.NewBuffer(nil)
 
+	index := os.Getenv("ES_INDEX")
+
 	timer := time.NewTimer(20 * time.Second)
 
 	go func() {
 		for range timer.C {
 			if songBuffer.Len() != 0 {
-				flush(songBuffer)
+				flush(songBuffer, index)
 				counter = 0
 			}
 		}
@@ -40,7 +42,7 @@ func SongStore(batchSize int) {
 
 		if counter >=  batchSize {
 			// flush to db
-			flush(songBuffer)
+			flush(songBuffer, index)
 			counter = 0
 		} else {
 			meta := []byte(fmt.Sprintf(`{ "index" : { }%s`, "\n"))
@@ -51,9 +53,9 @@ func SongStore(batchSize int) {
 	}
 }
 
-func flush(buffer *bytes.Buffer) {
+func flush(buffer *bytes.Buffer, index string) {
 	fmt.Printf("\"flushing to db\": %v\n", "flushing to db")
-	res, e := database.ES.Bulk(bytes.NewReader(buffer.Bytes()), database.ES.Bulk.WithIndex(os.Getenv("ES_INDEX")))
+	res, e := database.ES.Bulk(bytes.NewReader(buffer.Bytes()), database.ES.Bulk.WithIndex(index))
 	if e != nil {
 		fmt.Println("failed to insert items", e)
 	}
